f4cba74a5bac7142f32f11f6cdb6f3d7a6c0ad35
Michal Vystrk <d22346@oaopava.cz>
2025-03-28 11:56:45 +0100

    funguje to

--- project 1/rockPaperScissorsApp/views.py
+++ project 1/rockPaperScissorsApp/views.py
@@ -3,8 +3,21 @@ import random
 
 def home(request):
     result = None
+    userChoice = None
+    computerChoice = None
     if request.method == 'POST':
-        user_choice = request.POST['weapon']
-        computer_choice = random.choice(['rockPaperScissorsApp/images/gameSighns/Rock.png', 'rockPaperScissorsApp/images/gameSighns/Paper.png', 'rockPaperScissorsApp/images/gameSighns/Scissors.png'])
-    return render(request, 'rockPaperScissorsApp/home.html', {'userChoice': user_choice, 'computerChoice': computer_choice})
-    
+        userChoice = request.POST['weapon']
+        computerChoice = random.choice(['rockPaperScissorsApp/images/gameSighns/Rock.png', 'rockPaperScissorsApp/images/gameSighns/Paper.png', 'rockPaperScissorsApp/images/gameSighns/Scissors.png'])
+    result = determanWinner(userChoice, computerChoice)
+    return render(request, 'rockPaperScissorsApp/home.html', {'userChoice': userChoice, 'computerChoice': computerChoice})
+
+def determanWinner(userChoice, computerChoice):
+    if userChoice == computerChoice:
+        return 'It\'s a tie!'
+    if userChoice == 'rock' and computerChoice == 'scissors':
+        return 'You win!'
+    if userChoice == 'paper' and computerChoice == 'rock':
+        return 'You win!'
+    if userChoice == 'scissors' and computerChoice == 'paper':
+        return 'You win!'
+    return 'You lose!'
